b65cc72d76e233e8c91d8fe2e6358de171e2b9ad
CTtao <1838346304@qq.com>
2023-04-23 19:06:25 +0800

    4.2 测试自定义网络传输协议拼接

---
+++ ctrpc-protocol/src/test/java/Test.java
@@ -0,0 +1,28 @@
+import com.ct.rpc.protocol.RpcProtocol;
+import com.ct.rpc.protocol.header.RpcHeader;
+import com.ct.rpc.protocol.header.RpcHeaderFactory;
+import com.ct.rpc.protocol.request.RpcRequest;
+
+/**
+ * @author CT
+ * @version 1.0.0
+ * @description
+ */
+public class Test {
+    public static RpcProtocol<RpcRequest> getProtocol(){
+        RpcHeader header = RpcHeaderFactory.getRequestHeader("jdk");
+        RpcRequest body = new RpcRequest();
+        body.setOneway(false);
+        body.setAsync(false);
+        body.setClassName("com.ct.rpc.demo.RpcProtocol");
+        body.setMethodName("hello");
+        body.setGroup("ct");
+        body.setParameters(new Object[]{"ct"});
+        body.setParameterTypes(new Class[]{String.class});
+        body.setVersion("1.0.0");
+        RpcProtocol<RpcRequest> protocol = new RpcProtocol<>();
+        protocol.setBody(body);
+        protocol.setHeader(header);
+        return protocol;
+    }
+}
