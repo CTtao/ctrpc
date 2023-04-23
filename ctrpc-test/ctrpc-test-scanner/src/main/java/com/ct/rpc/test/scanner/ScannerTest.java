adde6850695372a6de01f6efd17ac1ad21f127d8
CTtao <1838346304@qq.com>
2023-04-23 10:42:02 +0800

    2.* 重构项目模块结构

--- ctrpc-test/ctrpc-test-scanner/src/main/java/com/ct/rpc/test/scanner/ScannerTest.java
+++ ctrpc-test/ctrpc-test-scanner/src/main/java/com/ct/rpc/test/scanner/ScannerTest.java
@@ -3,6 +3,7 @@ package com.ct.rpc.test.scanner;
 import com.ct.rpc.common.scanner.ClassScanner;
 import com.ct.rpc.common.scanner.reference.RpcReferenceScanner;
 import com.ct.rpc.common.scanner.server.RpcServiceScanner;
+import org.junit.Test;
 
 import java.util.List;
 
@@ -12,15 +13,12 @@ import java.util.List;
  * @description
  */
 public class ScannerTest {
-    public static void main(String[] args) throws Exception{
-        ScannerTest scannerTest = new ScannerTest();
-        scannerTest.testScannerClassNameListByRpcReference();
-    }
 
     /**
      * 扫描com.ct.rpc.test.scanner包下的所有类
      * @throws Exception
      */
+    @Test
     public void testScannerClassNameList() throws Exception{
         List<String> classNameList = ClassScanner.getClassNameList("com.ct.rpc.test.scanner");
         classNameList.forEach(System.out::println);
@@ -30,10 +28,12 @@ public class ScannerTest {
      * 扫描com.ct.rpc.test.scanner包下所有标注了@RpcService注解的类
      * @throws Exception
      */
+    @Test
     public void testScannerClassNameListByRpcService() throws Exception{
         RpcServiceScanner.doScannerWithRpcServiceAnnotationFilterAndRegistryService("com.ct.rpc.test.scanner");
     }
 
+    @Test
     public void testScannerClassNameListByRpcReference() throws Exception{
         RpcReferenceScanner.doScannerWithRpcReferenceAnnotationFilter("com.ct.rpc.test.scanner");
     }
