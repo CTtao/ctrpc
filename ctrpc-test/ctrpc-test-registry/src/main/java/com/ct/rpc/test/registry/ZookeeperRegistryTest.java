8ea53895a81ee929579a4783db924f4a8afdf2c1
CTtao <1838346304@qq.com>
2023-05-06 21:57:08 +0800

    21 注册中心基础服务功能实现

---
+++ ctrpc-test/ctrpc-test-registry/src/main/java/com/ct/rpc/test/registry/ZookeeperRegistryTest.java
@@ -0,0 +1,48 @@
+package com.ct.rpc.test.registry;
+
+import com.ct.rpc.protocol.meta.ServiceMeta;
+import com.ct.rpc.registry.api.RegistryService;
+import com.ct.rpc.registry.api.config.RegistryConfig;
+import com.ct.rpc.registry.zookeeper.ZookeeperRegistryService;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author CT
+ * @version 1.0.0
+ * @description
+ */
+public class ZookeeperRegistryTest {
+    private RegistryService registryService;
+    private ServiceMeta serviceMeta;
+
+    @Before
+    public void init() throws Exception{
+        RegistryConfig registryConfig = new RegistryConfig("127.0.0.1:2181", "zookeeper");
+        this.registryService = new ZookeeperRegistryService();
+        this.registryService.init(registryConfig);
+        this.serviceMeta = new ServiceMeta(ZookeeperRegistryTest.class.getName(), "1.0.0","ct","127.0.0.1",8080);
+    }
+
+    @Test
+    public void testRegister() throws Exception{
+        this.registryService.register(serviceMeta);
+    }
+
+    @Test
+    public void testUnregister() throws Exception{
+        this.registryService.unregister(serviceMeta);
+    }
+
+    @Test
+    public void testDiscovery() throws Exception{
+        this.registryService.discovery(RegistryService.class.getName(), "ct".hashCode());
+    }
+
+    @Test
+    public void testDestroy() throws IOException {
+        this.registryService.destroy();
+    }
+}
