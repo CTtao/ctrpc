fda2d9051bc99673be1ecfb52a867da5f1e0a0af
CTtao <1838346304@qq.com>
2023-05-08 23:04:40 +0800

    30 基于SPI扩展Kryo序列化与反序列化机制

--- ctrpc-test/ctrpc-test-consumer/ctrpc-test-consumer-native/src/main/java/com/ct/rpc/test/consumer/RpcConsumerNativeTest.java
+++ ctrpc-test/ctrpc-test-consumer/ctrpc-test-consumer-native/src/main/java/com/ct/rpc/test/consumer/RpcConsumerNativeTest.java
@@ -33,7 +33,7 @@ public class RpcConsumerNativeTest {
                 "zookeeper",
                 "1.0.0",
                 "ct",
-                "fst",
+                "kryo",
                 3000,
                 false,
                 false);
