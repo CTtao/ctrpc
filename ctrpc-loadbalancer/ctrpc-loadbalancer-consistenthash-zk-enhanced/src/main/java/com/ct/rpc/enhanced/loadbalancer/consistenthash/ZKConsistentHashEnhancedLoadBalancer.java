704d03d7de626c48918c4b1cac04b7b090ce4591
CTtao <1838346304@qq.com>
2023-05-14 16:53:33 +0800

    55 基于SPI扩展增强型Zookeeper一致性Hash算法负载均衡策略

---
+++ ctrpc-loadbalancer/ctrpc-loadbalancer-consistenthash-zk-enhanced/src/main/java/com/ct/rpc/enhanced/loadbalancer/consistenthash/ZKConsistentHashEnhancedLoadBalancer.java
@@ -0,0 +1,58 @@
+package com.ct.rpc.enhanced.loadbalancer.consistenthash;
+
+import com.ct.rpc.loadbalancer.base.BaseEnhancedServiceLoadBalancer;
+import com.ct.rpc.protocol.meta.ServiceMeta;
+import com.ct.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author CT
+ * @version 1.0.0
+ * @description
+ */
+@SPIClass
+public class ZKConsistentHashEnhancedLoadBalancer extends BaseEnhancedServiceLoadBalancer {
+
+    private final static int VIRTUAL_NODE_SIZE = 10;
+    private final static String VIRTUAL_NODE_SPLIT = "#";
+
+    private final Logger logger = LoggerFactory.getLogger(ZKConsistentHashEnhancedLoadBalancer.class);
+
+    @Override
+    public ServiceMeta select(List<ServiceMeta> servers, int hashCode, String sourceIP) {
+        logger.info("基于Zookeeper增强型的一致性Hash算法的负载均衡策略...");
+        TreeMap<Integer, ServiceMeta> ring = makeConsistentHashRing(servers);
+        return allocateNode(ring, hashCode);
+    }
+
+    private ServiceMeta allocateNode(TreeMap<Integer, ServiceMeta> ring, int hashCode) {
+        Map.Entry<Integer, ServiceMeta> entry = ring.ceilingEntry(hashCode);
+        if (entry == null) {
+            entry = ring.firstEntry();
+        }
+        if (entry == null){
+            throw new RuntimeException("not discover useful service, please register service in registry center.");
+        }
+        return entry.getValue();
+    }
+
+    private TreeMap<Integer, ServiceMeta> makeConsistentHashRing(List<ServiceMeta> servers) {
+        TreeMap<Integer, ServiceMeta> ring = new TreeMap<>();
+        for (ServiceMeta instance : servers) {
+            for (int i = 0; i < VIRTUAL_NODE_SIZE; i++) {
+                ring.put((buildServiceInstanceKey(instance) + VIRTUAL_NODE_SPLIT + i).hashCode(), instance);
+            }
+        }
+        return ring;
+    }
+
+    private String buildServiceInstanceKey(ServiceMeta instance) {
+        return String.join(":", instance.getServiceAddr(), String.valueOf(instance.getServicePort()));
+    }
+
+}
