10e29cc7a59522326dded3e120b062321efdf6a9
CTtao <1838346304@qq.com>
2023-07-02 18:26:49 +0800

    77 基于SpringBoot接入服务消费者

---
+++ ctrpc-demo/ctrpc-demo-spring-boot-consumer/src/main/java/com/ct/rpc/demo/spring/boot/consumer/SpringBootConsumerDemoStarter.java
@@ -0,0 +1,27 @@
+package com.ct.rpc.demo.spring.boot.consumer;
+
+import com.ct.rpc.demo.spring.boot.consumer.service.ConsumerDemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author CT
+ * @version 1.0.0
+ * @description
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ct.rpc"})
+public class SpringBootConsumerDemoStarter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootConsumerDemoStarter.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootConsumerDemoStarter.class, args);
+        ConsumerDemoService consumerDemoService = context.getBean(ConsumerDemoService.class);
+        String result = consumerDemoService.hello("ct");
+        LOGGER.info("返回的结果数据===>>> " + result);
+    }
+}
---
+++ ctrpc-demo/ctrpc-demo-spring-boot-consumer/src/main/java/com/ct/rpc/demo/spring/boot/consumer/service/ConsumerDemoService.java
@@ -0,0 +1,10 @@
+package com.ct.rpc.demo.spring.boot.consumer.service;
+
+/**
+ * @author CT
+ * @version 1.0.0
+ * @description 服务消费者Service接口
+ */
+public interface ConsumerDemoService {
+    String hello(String name);
+}
