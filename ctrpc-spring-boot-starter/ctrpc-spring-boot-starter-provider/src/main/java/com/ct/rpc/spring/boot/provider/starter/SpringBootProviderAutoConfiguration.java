9c3cda5f7d599b51a8dc4af3aed9468ec7ca9a29
CTtao <1838346304@qq.com>
2023-07-02 17:51:47 +0800

    74 服务提供者整合SpringBoot

---
+++ ctrpc-spring-boot-starter/ctrpc-spring-boot-starter-provider/src/main/java/com/ct/rpc/spring/boot/provider/starter/SpringBootProviderAutoConfiguration.java
@@ -0,0 +1,32 @@
+package com.ct.rpc.spring.boot.provider.starter;
+
+import com.ct.rpc.provider.spring.RpcSpringServer;
+import com.ct.rpc.spring.boot.provider.config.SpringBootProviderConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author CT
+ * @version 1.0.0
+ * @description
+ */
+@Configuration
+public class SpringBootProviderAutoConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "ctrpc.ct.provider")
+    public SpringBootProviderConfig springBootProviderConfig(){
+        return new SpringBootProviderConfig();
+    }
+
+    @Bean
+    public RpcSpringServer rpcSpringServer(final SpringBootProviderConfig springBootProviderConfig){
+        return new RpcSpringServer(springBootProviderConfig.getServerAddress(),
+                springBootProviderConfig.getRegistryAddress(),
+                springBootProviderConfig.getRegistryType(),
+                springBootProviderConfig.getRegistryLoadBalanceType(),
+                springBootProviderConfig.getReflectType(),
+                springBootProviderConfig.getHeartbeatInterval(),
+                springBootProviderConfig.getScanNotActiveChannelInterval());
+    }
+}
