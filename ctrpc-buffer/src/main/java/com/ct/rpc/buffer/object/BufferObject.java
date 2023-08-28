264ca9099f534c707dac2d829c5581e43b977526
CTtao <1838346304@qq.com>
2023-08-28 19:11:45 +0800

    103 数据缓冲基础模型设计

---
+++ ctrpc-buffer/src/main/java/com/ct/rpc/buffer/object/BufferObject.java
@@ -0,0 +1,45 @@
+package com.ct.rpc.buffer.object;
+
+import com.ct.rpc.protocol.RpcProtocol;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.io.Serializable;
+
+/**
+ * @author CT
+ * @version 1.0.0
+ * @description
+ */
+public class BufferObject<T> implements Serializable {
+    private static final long serialVersionUID = -5465112244213170405L;
+
+    //Netty读写数据的ChannelHandlerContext
+    private ChannelHandlerContext ctx;
+
+    //网络传输协议对象
+    private RpcProtocol<T> protocol;
+
+    public BufferObject() {
+    }
+
+    public BufferObject(ChannelHandlerContext ctx, RpcProtocol<T> protocol) {
+        this.ctx = ctx;
+        this.protocol = protocol;
+    }
+
+    public ChannelHandlerContext getCtx() {
+        return ctx;
+    }
+
+    public void setCtx(ChannelHandlerContext ctx) {
+        this.ctx = ctx;
+    }
+
+    public RpcProtocol<T> getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(RpcProtocol<T> protocol) {
+        this.protocol = protocol;
+    }
+}
