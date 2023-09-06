73347f3d2e26e5d2c3cd9c3a99b364b2860d725f
CTtao <1838346304@qq.com>
2023-09-06 20:21:04 +0800

    116 服务熔断基础模型设计

---
+++ ctrpc-fusing/ctrpc-fusing-counter/src/main/java/com/ct/rpc/fusing/counter/CounterFusingInvoker.java
@@ -0,0 +1,89 @@
+package com.ct.rpc.fusing.counter;
+
+import com.ct.rpc.constants.RpcConstants;
+import com.ct.rpc.fusing.base.AbstractFusingInvoker;
+import com.ct.rpc.spi.annotation.SPIClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author CT
+ * @version 1.0.0
+ * @description 在一段时间内基于错误数量的熔断策略
+ */
+@SPIClass
+public class CounterFusingInvoker extends AbstractFusingInvoker {
+    private final Logger logger = LoggerFactory.getLogger(CounterFusingInvoker.class);
+    @Override
+    public boolean invokeFusingStrategy() {
+        logger.info("execute counter fusing strategy, current fusing status is {}", fusingStatus.get());
+        switch (fusingStatus.get()){
+            //关闭状态
+            case RpcConstants.FUSING_STATUS_CLOSED:
+                return invokeClosedFusingStrategy();
+            //半开启状态
+            case RpcConstants.FUSING_STATUS_HALF_OPEN:
+                return invokeHalfOpenFusingStrategy();
+            //开启状态
+            case RpcConstants.FUSING_STATUS_OPEN:
+                return invokeOpenFusingStrategy();
+            default:
+                return invokeClosedFusingStrategy();
+        }
+    }
+
+
+    /**
+     * 处理开启状态逻辑
+     */
+    private boolean invokeOpenFusingStrategy(){
+        //获取当前时间
+        long currentTimeStamp = System.currentTimeMillis();
+        //超过一个指定的时间范围，则将状态设置为半开启状态
+        if (currentTimeStamp - lastTimeStamp >= milliseconds){
+            fusingStatus.set(RpcConstants.FUSING_STATUS_HALF_OPEN);
+            lastTimeStamp = currentTimeStamp;
+            this.resetCount();
+            return false;
+        }
+        return true;
+    }
+    /**
+     * 处理半开启状态逻辑
+     */
+    private boolean invokeHalfOpenFusingStrategy(){
+        //获取当前时间
+        long currentTimeStamp = System.currentTimeMillis();
+        //服务已恢复
+        if (currentFailureCounter.get() <= 0){
+            fusingStatus.set(RpcConstants.FUSING_STATUS_CLOSED);
+            lastTimeStamp = currentTimeStamp;
+            this.resetCount();
+            return false;
+        }
+        //服务未恢复
+        fusingStatus.set(RpcConstants.FUSING_STATUS_OPEN);
+        lastTimeStamp = currentTimeStamp;
+        return true;
+    }
+    /**
+     * 处理关闭状态逻辑
+     */
+    private boolean invokeClosedFusingStrategy(){
+        //获取当前时间
+        long currentTimeStamp = System.currentTimeMillis();
+        //超过一个指定的时间范围
+        if (currentTimeStamp - lastTimeStamp >= milliseconds){
+            lastTimeStamp = currentTimeStamp;
+            this.resetCount();
+            return false;
+        }
+
+        if (currentFailureCounter.get() >= totalFailure){
+            lastTimeStamp = currentTimeStamp;
+            fusingStatus.set(RpcConstants.FUSING_STATUS_OPEN);
+            return true;
+        }
+        return false;
+    }
+}
